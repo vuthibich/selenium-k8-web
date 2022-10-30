3ccd3e2dd519ccdd93647fa4904194f02085fcb2
Vu Bich <bichvt141295@gmail.com>
2022-10-30 19:14:17 +0700

    lesson-30

---
+++ src/main/java/api_learning/testng/TestNGTestOrder.java
@@ -0,0 +1,18 @@
+package api_learning.testng;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestNGTestOrder {
+
+    @Test(priority = 1, dependsOnMethods = {"test01"})
+    public void test02(){
+        System.out.println("Test 02");
+    }
+
+    @Test(priority = 2)
+    public void test01(){
+        Assert.fail();
+        System.out.println("Test 01");
+    }
+}
