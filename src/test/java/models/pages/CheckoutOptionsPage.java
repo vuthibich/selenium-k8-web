5095b15de92d07b3ce11b80a418c9fb64cdb9b26
Vu Bich <bichvt141295@gmail.com>
2022-10-30 18:39:08 +0700

    lesson-27

---
+++ src/test/java/models/pages/CheckoutOptionsPage.java
@@ -0,0 +1,18 @@
+package models.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CheckoutOptionsPage extends BasePage {
+
+    private static final By checkoutAsGuestBtn = By.cssSelector(".checkout-as-guest-button");
+
+    public CheckoutOptionsPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void checkoutAsGuest(){
+        component.findElement(checkoutAsGuestBtn).click();
+    }
+
+}
