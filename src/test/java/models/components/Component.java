2a9edff7956c4227d5009eed453080a2dd5464a0
Vu Bich <bichvt141295@gmail.com>
2022-10-30 18:09:05 +0700

    lesson-26

--- src/test/java/models/components/Component.java
+++ src/test/java/models/components/Component.java
@@ -1,6 +1,7 @@
 package models.components;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -84,5 +85,17 @@ public class Component {
             throw new IllegalArgumentException("Component class " + componentClass + " must have annotation "
                     + ComponentCssSelector.class.getSimpleName() + " or " + ComponentXpathSelector.class.getSimpleName());
         }
+    }
+
+    public void scrollUpToElement(WebElement element){
+        scrollToElement("false", element);
+    }
+
+    public void scrollDownToElement(WebElement element){
+        scrollToElement("true", element);
+    }
+
+    private void scrollToElement(String position, WebElement element){
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(" + position + ");", element);
     }
 }
--- src/test/java/test/computer/BuyingCheapComputerTest.java
+++ src/test/java/test/computer/BuyingCheapComputerTest.java
@@ -18,6 +18,7 @@ public class BuyingCheapComputerTest extends BaseTest implements Urls {
                 new OrderComputerFlow<>(driver, CheapComputerComponent.class, computerData);
 
         orderComputerFlow.buildCompSpecAndAddToCart();
+        orderComputerFlow.verifyShoppingCartPage();
     }
 
     @DataProvider
